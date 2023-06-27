d64571448b0673e068fe65156715ae7e8857feec
Amanda Nurul Izzah <amanda.nurul11@ui.ac.id>
2023-06-27 21:35:31 +0700

    initial commit

---
+++ src/main/java/com/example/murialiving/MurialivingApplication.java
@@ -0,0 +1,13 @@
+package com.example.murialiving;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MurialivingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MurialivingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/murialiving/MurialivingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.murialiving;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MurialivingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
