a94f93511daec01a83526a863808f15d547f7025
amrul-hzz <amanda.nurul11@ui.ac.id>
2023-07-01 11:24:52 +0700

    add user service implementation

--- src/main/java/com/example/murialiving/service/UserService.java
+++ src/main/java/com/example/murialiving/service/UserService.java
@@ -1,11 +1,10 @@
 import org.springframework.stereotype.Service;
+
 @Service
 public interface UserService {
-
     <List> getAllUsers();
     User findUserByUsername(String username);
     User createUser(UserRequest request);
     User updateUserByRoomNumber(String roomNo, UserRequest request);
     void deleteUserByRoomNo(String roomNo);
-
 }
---
+++ src/main/java/com/example/murialiving/service/UserServiceImpl.java
@@ -0,0 +1,47 @@
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import java.util.*;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+@Service
+@RequiredArgsConstructor
+public class UserServiceImpl implements UserService {
+    private final UserRepository userRepository;
+    @Override
+    public <List> getAllUsers() {
+        return userRepository.getAllUsers();
+    }
+
+    @Override
+    public User findUserByUsername(String username) {
+        return userRepository.findUserByUsername(username);
+    }
+
+    @Override
+    public User createUser(UserRequest request){
+        // TODO: validateUserRequest(request);
+        User user = user.builder()
+                .roomNo(request.getRoomNo())
+                .username(request.getUsername())
+                .password(request.getPassword())
+                .build();
+        return userRepository.save(user);
+    }
+
+    @Override
+    public User updateUserByRoomNumber(String roomNo, UserRequest request){
+        User userUpdated = user.builder()
+                .roomNo(roomNo)
+                .username(request.getUsername())
+                .password(request.getPassword())
+                .build();
+        return userRepository.save(userUpdated);
+    }
+
+    @Override
+    public void deleteUserByRoomNo(String roomNo) {
+        userRepository.deleteUserByRoomNo(roomNo);
+    }
+
+}
